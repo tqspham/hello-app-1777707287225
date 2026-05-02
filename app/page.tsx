a893d24eec1885f2fded886ccc1091407878ba29
tqspham <tqs.pham@gmail.com>
2026-05-02 09:35:07 +0200

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
+      <div className="text-center">
+        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-indigo-900 mb-8">
+          Hello App
+        </h1>
+        <p className="text-2xl sm:text-3xl lg:text-4xl text-indigo-700 font-semibold">
+          Hello
+        </p>
+      </div>
+    </div>
+  );
+}
